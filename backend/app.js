81c8965a64c6266d8d3381b449a23342fc19b156
Yago Emanuel B. De Moura <yagoemanuel99@gmail.com>
2026-05-06 20:59:50 -0300

    corrigir erro ao iniciar servidor

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,26 @@
-const express = require('express')
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+//middleware globais do projetos completo
+app.use(cors())
+app.use(express.json())//usando para mostrar os erros atravez dos JSON
+
+//importando as rotas
+
+const userRoutes = require("./src/modules/users/user.routes")
+
+//usando as rotas
+app.use('/user', userRoutes)
+
+//rota de teste
+app.get('/teste', (req, res) => {
+    res.send('API esta rodando')
+})
+
+
+//iniciando o servidor 
+app.listen(process.env.PORT || 8081, () => {
+    console.log(`servidor rodando na porta ${PORT || 8081}`)
+})
--- backend/src/modules/users/user.routes.js
+++ backend/src/modules/users/user.routes.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const userController = require("./user.controller")
-const authMiddleware = require("../middleware")
+const authMiddleware = require("../../../middleware")
 
-router.post()
+router.get("/users", authMiddleware, userController.userController.getAll)
+router.post("/users", authMiddleware, userController.userController.createUser)
+
+module.exports = {
+    router
+}
